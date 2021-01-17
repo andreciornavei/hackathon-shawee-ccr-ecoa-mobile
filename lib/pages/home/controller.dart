8bfd706d3981b22de57a14d0340c732cbc7891e6
Andre Ciornavei <andre.ciornavei@gmail.com>
2021-01-17 13:21:24 -0300

    implemented banners on home screen

--- lib/pages/home/controller.dart
+++ lib/pages/home/controller.dart
@@ -11,8 +11,11 @@ import 'widgets/category_card.dart';
 class HomeController extends GetxController {
   List<HomeItemModel> items = [
     HomeItemModel(
-      type: 'h1',
-      data: {"label": "Categorias"},
+      type: 'slider',
+      data: [
+        "https://img-a.udemycdn.com/notices/home_banner/image_udlite/8a5d045c-2dd2-4a4d-bb0e-a487af8a5aa0.jpg?1Y3N7IUFC64ibPTahpllz9x6bv8l4xVnL1P2jecEt3p-kKcoVBrakvoP6USieAR8wutajVQlT1Tk7KwKS67KTHYx0HMdRTvcsDseSmrGkgFhXOTWRXEepqhADD8E_shhOWHQRyyFppUfy7HH3vZnw3WFOxYJYKMITTKLm0H_WkLeHeyDkN0EjA",
+        "https://a.omappapi.com/users/00c080c442d4/images/1ccaed429e1c1610559966-imagem-popup.png",
+      ],
     ),
     HomeItemModel(
       type: 'categories',
--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -1,5 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecoar_mobile/models/homeitem.model.dart';
 import 'package:ecoar_mobile/pages/home/widgets/course_card.dart';
+import 'package:ecoar_mobile/utils/colors.dart';
+import 'package:ecoar_mobile/widgets/card.dart';
 import 'package:ecoar_mobile/widgets/h1.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -23,7 +27,8 @@ class HomeView extends StatelessWidget {
             child: ListView.separated(
               scrollDirection: Axis.horizontal,
               padding: EdgeInsets.symmetric(horizontal: 25),
-              itemBuilder: (BuildContext context, int index) => item?.data[index],
+              itemBuilder: (BuildContext context, int index) =>
+                  item?.data[index],
               separatorBuilder: (context, index) => SizedBox(width: 20),
               itemCount: item?.data?.length ?? 0,
             ),
@@ -38,6 +43,42 @@ class HomeView extends StatelessWidget {
                       {Get.toNamed("/course/${item?.data[index].id}")});
             },
             childCount: item?.data?.length ?? 0,
+          ),
+        );
+      case 'slider':
+        return SliverToBoxAdapter(
+          child: CarouselSlider.builder(
+            options: CarouselOptions(
+              height: 200.0,
+              viewportFraction: 0.9,
+              enableInfiniteScroll: false,
+              enlargeCenterPage: true,
+            ),
+            itemBuilder: (context, index) => Container(
+              margin: EdgeInsets.symmetric(horizontal: 0.0),
+              padding: EdgeInsets.only(bottom: 25, top: 25),
+              child: Container(
+                  width: double.maxFinite,
+                  margin: EdgeInsets.zero,
+                  padding: EdgeInsets.symmetric(vertical: 5),
+                  decoration: BoxDecoration(
+                    color: AppColors.WHITE,
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        blurRadius: 15,
+                        color: AppColors.SHADOW,
+                        offset: Offset(6, 10),
+                      ),
+                    ],
+                    image: DecorationImage(
+                      image: CachedNetworkImageProvider(item?.data[index]),
+                      fit: BoxFit.cover,
+                      alignment: Alignment.center,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(6)),
+                  )),
+            ),
+            itemCount: item?.data?.length ?? 0,
           ),
         );
     }
